55036647f0cc12c9f61d328c6852e9823ffface3
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-10-02 00:20:41 +0530

    modified algorithm and added HomePage Contents

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,14 @@
+import Footer from "../components/Footer";
 import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+import HomeContent from "../components/HomeContent";
 
 export default function Home() {
     return (
         <Layout title="Home" content="This is the homepage">
-            Home
+           <Navbar/>
+           <HomeContent/>
+           <Footer/>
         </Layout>
     );
 }
