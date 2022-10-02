016786f7e08784bfe190c063cdafb26d4ace0b46
SarveshPatil29 <sarvesh2902@gmail.com>
2022-10-02 07:55:49 +0530

    added backend for weather

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,13 @@
 from __future__ import print_function
 import json
-import requests
-from fpdf import FPDF
-from flask import Flask,request,render_template
-import numpy as np
 
+# import requests
+from fpdf import FPDF
+from flask import Flask, request, render_template
+import numpy as np
 import pickle
 
-
-
 app = Flask(__name__)
-
 
 # prediction function
 def CropPredictor(to_predict_list):
@@ -19,42 +16,43 @@ def CropPredictor(to_predict_list):
     result = loaded_model.predict(to_predict)
     return result[0]
 
+
 def FertilizerPredictor(to_predict_list):
     to_predict = np.array([to_predict_list])
     loaded_model = pickle.load(open("models/classifier.pkl", "rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
 
-@app.route('/cropPredict', methods = ['POST'])
+
+def WeatherPredictor(to_predict_list):
+    to_predict = np.array([to_predict_list])
+    loaded_model = pickle.load(open("models/weather.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route("/cropPredict", methods=["POST"])
 def result():
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.json)
         to_predict_list = request.json
-
         to_predict_list = list(to_predict_list.values())
-
         to_predict_list = list(map(int, to_predict_list))
-
         result = CropPredictor(to_predict_list)
-
         return result
 
 
-@app.route('/fertilizerPredict', methods = ['POST'])
+@app.route("/fertilizerPredict", methods=["POST"])
 def result2():
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.json)
         to_predict_list = request.json
-
         to_predict_list = list(to_predict_list.values())
-
         to_predict_list = list(map(int, to_predict_list))
-
         ans = FertilizerPredictor(to_predict_list)
-
         if ans == 0:
             return "10-26-26"
-        elif ans ==1:
+        elif ans == 1:
             return "14-35-14"
         elif ans == 2:
             return "17-17-17	"
@@ -67,6 +65,17 @@ def result2():
         else:
             return "Urea"
 
+
+@app.route("/weather-predict", methods=["POST"])
+def result3():
+    if request.method == "POST":
+        print(request.json)
+        to_predict_list = request.json
+        to_predict_list = list(to_predict_list.values())
+        print(to_predict_list)
+        weather = WeatherPredictor(to_predict_list)
+        result = {"data": weather}
+        return result
 
 
 if __name__ == "__main__":
